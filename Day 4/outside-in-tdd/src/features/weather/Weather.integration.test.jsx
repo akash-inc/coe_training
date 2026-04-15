cabbb608e9b4ccd041dff0f8007571d23e63eb0f
akash-inc <akash.p@incubyte.co>
2026-04-15 14:54:31 +0530

    [ADD] Day 4: weather feature integration tests

---
+++ Day 4/outside-in-tdd/src/features/weather/Weather.integration.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { WeatherFeature } from './WeatherFeature'
+import { server } from '../../mocks/server'
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test('user can search for weather in a city and see the temperature', async () => {
+  const user = userEvent.setup()
+  render(<WeatherFeature />)
+
+  await user.type(screen.getByRole('textbox', { name: /city/i }), 'London')
+  await user.click(screen.getByRole('button', { name: /search/i }))
+
+  expect(await screen.findByText(/london/i)).toBeInTheDocument()
+  expect(await screen.findByText(/20°c/i)).toBeInTheDocument()
+})
