864e34823ac707e3b9bc17fb496237db8d3a9a22
akash-inc <akash.p@incubyte.co>
2026-04-15 15:55:11 +0530

    [FIX] Day 4: useWeather tests with MSW for API mocking

--- Day 4/outside-in-tdd/src/features/weather/useWeather.test.js
+++ Day 4/outside-in-tdd/src/features/weather/useWeather.test.js
@@ -1,5 +1,15 @@
 import { renderHook, act } from '@testing-library/react'
+import { afterAll, afterEach, beforeAll } from 'vitest'
+import { server } from '../../mocks/server'
 import { useWeather } from './useWeather'
+
+beforeAll(() => server.listen())
+
+afterEach(() => {
+  server.resetHandlers()
+})
+
+afterAll(() => server.close())
 
 test('fetches weather for a city and returns formatted data', async () => {
   const { result } = renderHook(() => useWeather())
