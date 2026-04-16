35c4b14479da54a9eb85589b1720cdd4320e45ad
akash-inc <akash.p@incubyte.co>
2026-04-16 11:18:56 +0530

    [IMP] Day 4: implement ComboboxInput component with accessible input and label

--- Day 4/accessible-outside-in-tdd/src/features/combobox/components/ComboboxInput.jsx
+++ Day 4/accessible-outside-in-tdd/src/features/combobox/components/ComboboxInput.jsx
@@ -1,3 +1,28 @@
-export function ComboboxInput() {
-  return null;
+export function ComboboxInput({
+  inputId,
+  label,
+  value,
+  isExpanded,
+  controlsId,
+  activeDescendantId,
+  onChange,
+  onKeyDown,
+}) {
+  return (
+    <>
+      <label htmlFor={inputId}>{label}</label>
+      <input
+        id={inputId}
+        type="text"
+        role="combobox"
+        value={value}
+        aria-autocomplete="list"
+        aria-expanded={isExpanded ? 'true' : 'false'}
+        aria-controls={isExpanded ? controlsId : undefined}
+        aria-activedescendant={activeDescendantId}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+      />
+    </>
+  )
 }
